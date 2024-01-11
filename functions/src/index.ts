0b52ecf396570b878b0d8f645a68b98660bc4164
Cody <66153585+nonstickcode@users.noreply.github.com>
2024-01-10 21:11:44 -0700

    commit

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,3 +3,5 @@ import * as functions from "firebase-functions";
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase! This finally works!!!!!");
 });
+
+// comment
