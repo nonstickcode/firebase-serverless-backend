fea801a648753efac93e675dab6af2d3532fa08a
Cody <66153585+nonstickcode@users.noreply.github.com>
2024-01-10 22:04:43 -0700

    good for now, need to try promises next

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# to deploy just enter "firebase deploy" 
+# to deploy just enter - "**firebase deploy**" 
 
 - to rebuild functions when changed.
+
+- view logs in CLI with enter -  "**firebase functions:log**"
 
 - https://us-central1-route-test-with-firebase.cloudfunctions.net/helloWorld is current route
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,6 @@
 import * as functions from "firebase-functions";
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase! This finally works!!!!!");
+  console.log("did this show up in logs?");
+  response.send("Hello from Firebase! YAY!!!!!");
 });
-
-// comment
